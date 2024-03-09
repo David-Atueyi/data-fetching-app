6400ffeae424c440ec7f0f4a00f66aa4b7cf549e
David-Atueyi <atueyidavid2005@gmail.com>
2024-03-09 18:21:11 +0100

    finished the code base setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>boesdogjp;fsbgafod</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Hompage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Hompage = () => {
+  return (
+    <div>Hompage</div>
+  )
+}
